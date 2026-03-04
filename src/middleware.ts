c2dbf07c5185de40fcc3241d46ee7fd6351a59b7
Gasser Mahmoud Ahmed Mahmoud ELSayed <gogo05tv@gmail.com>
2026-03-04 17:11:07 +0200

    Fix the fix of the fix of the settings buttons. Configure basic PWA.

--- src/middleware.ts
+++ src/middleware.ts
@@ -46,5 +46,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|manifest.webmanifest|icon-.*\\.png|apple-touch-icon\\.png).*)",
+  ],
 };
