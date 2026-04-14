473f897c630e86c53bfa5cf863f21d179de08364
Gasser Mahmoud Ahmed Mahmoud ELSayed <gogo05tv@gmail.com>
2026-04-14 06:19:27 +0200

    still thinking

--- src/features/timeline/components/block.tsx
+++ src/features/timeline/components/block.tsx
@@ -1,6 +1,15 @@
-export default function Block() {
+export default function Block({
+  blockWidth,
+  blockHeight,
+}: {
+  blockWidth: number;
+  blockHeight: number;
+}) {
   return (
-    <div className="w-fit flex flex-col items-start">
+    <div
+      className="w-fit flex flex-col items-start"
+      style={{ maxWidth: `${blockWidth}px`, maxHeight: `${blockHeight}px` }}
+    >
       <span className="border-2 border-red-500 h-10 w-px"></span>
       <span className="border-2 border-gray-500 h-px w-16" />
     </div>
--- src/features/timeline/components/timelineWrapperPrototype.tsx
+++ src/features/timeline/components/timelineWrapperPrototype.tsx
@@ -3,39 +3,26 @@
 import ProposedBlock from "./proposedBlock";
 import Block from "./block";
 
+const blockWidth = 16 * 4;
+const blockHeight = 10 * 4;
+const eventSize = 7;
+
 export default function TimelineWrapperPrototype() {
   const events = 40;
-  const useProposedLayout = false;
-
-  const renderYourTimeline = () => (
+  const eventLength = blockWidth * eventSize; // px
+  return (
     <div className="relative w-full overflow-x-auto pb-10">
       <div className="absolute flex w-full gap-0">
         {Array.from({ length: events }).map((_, index) => (
-          <Block key={index} />
+          <Block
+            blockWidth={blockWidth}
+            blockHeight={blockHeight}
+            key={index}
+          />
         ))}
       </div>
-      <div className={`border h-4 w-${4 * 16}`} />
-      <div className="h-9.25 w-full" />
-      <div className="flex w-full">
-        {/* <span className="h-px w-full border-2" /> */}
-      </div>
+
+      <div className="border h-4" style={{ width: `${eventLength}px` }} />
     </div>
   );
-
-  const renderMyTimeline = () => (
-    <div className="w-full overflow-x-auto pb-10">
-      <div className="w-max min-w-full">
-        <div className="relative w-max min-w-full">
-          <div className="flex items-end justify-between gap-10">
-            {Array.from({ length: events }).map((_, index) => (
-              <ProposedBlock key={index} />
-            ))}
-          </div>
-          <div className="mt-0 h-px w-full bg-border" />
-        </div>
-      </div>
-    </div>
-  );
-
-  return useProposedLayout ? renderMyTimeline() : renderYourTimeline();
 }
